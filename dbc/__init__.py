0267f131ddaef8351056f606fc8e4dfd98e90f15
Cyril Adrian <cyril.adrian@gmail.com>
2011-12-02 07:27:44 +0100

    first commit of pyDBC

---
+++ dbc/__init__.py
@@ -0,0 +1,199 @@
+#  pyDBC: design by contract in Python
+#  Copyright (C) 2011 Cyril ADRIAN <cyril.adrian@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3 exclusively.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import inspect
+
+
+class ContractException(Exception): pass
+
+
+def checkAssertion(instance, tag, assertion, globals, locals):
+    if callable(assertion):
+        if assertion(instance):
+            return None
+        return tag
+
+    try:
+        assertionLocals = {"self": instance}
+        assertionLocals.update(locals)
+        if not eval(str(assertion), globals, assertionLocals):
+            return tag
+    except Exception, e:
+        return "%s - %s" % (tag, e)
+
+
+def checkAssertions(instance, assertions, globals, locals):
+    for tag, assertion in assertions.iteritems():
+        error = checkAssertion(instance, tag, assertion, globals, locals)
+        if error:
+            return error
+
+
+def checkInvariant(instance, cls):
+    for c in inspect.getmro(cls)[1::-1]:
+        error = checkAssertions(instance, c._invariants_, inspect.getmodule(c).__dict__, {})
+        if error:
+            raise ContractException(error)
+
+
+def checkPrecondition(instance, name, locals):
+    def check(cls):
+        if hasattr(cls, name):
+            c_feature = getattr(cls, name)
+            if hasattr(c_feature, "_preconditions"):
+                error = checkAssertions(instance, c_feature._preconditions, inspect.getmodule(cls).__dict__, locals)
+                if error:
+                    return error
+
+    checks = filter(lambda x: x is not None, map(check, inspect.getmro(instance.__class__)[1::-1]))
+    if len(checks):
+        raise ContractException("\n".join(checks))
+
+
+def globalsAndOld(globals, old):
+    result = {"old": old}
+    result.update(globals)
+    return result
+
+
+def checkPostcondition(instance, name, locals, oldValues):
+    def old(*a, **kw):
+        return oldValues.pop(0)
+
+    for cls in inspect.getmro(instance.__class__)[1::-1]:
+        if hasattr(cls, name):
+            c_feature = getattr(cls, name)
+            if hasattr(c_feature, "_postconditions"):
+                error = checkAssertions(instance, c_feature._postconditions, globalsAndOld(inspect.getmodule(cls).__dict__, old), locals)
+                if error:
+                    raise ContractException(error)
+
+
+def gatherPostconditionValue(result, instance, assertion, globals, locals):
+    try:
+        if callable(assertion):
+            assertion(instance)
+        else:
+            assertionLocals = {"self": instance}
+            assertionLocals.update(locals)
+            eval(str(assertion), globals, assertionLocals)
+    except:
+        pass
+
+
+def gatherPostconditionValues(result, instance, assertions, globals, locals):
+    for tag, assertion in assertions.iteritems():
+        gatherPostconditionValue(result, instance, assertion, globals, locals)
+
+
+def gatherOldValues(instance, name, locals):
+    result = []
+    def old(value):
+        result.append(value)
+
+    for cls in inspect.getmro(instance.__class__)[1::-1]:
+        if hasattr(cls, name):
+            c_feature = getattr(cls, name)
+            if hasattr(c_feature, "_postconditions"):
+                gatherPostconditionValues(result, instance, c_feature._postconditions, globalsAndOld(inspect.getmodule(cls).__dict__, old), locals)
+    return result
+
+
+def match(spec, instance, *a, **kw):
+    values = []
+    n = len(spec.args)
+    if n > 0 and spec.args[0] == 'self':
+        values.append(instance)
+        n = n - 1
+
+    values.extend(a)
+    n = n - len(a)
+
+    if spec.defaults is None:
+        values.extend([None] * n)
+    else:
+        values.extend([None] * (n - len(spec.defaults)))
+        values.extend(spec.defaults)
+
+    result = dict(zip(spec.args, values))
+    result.update(kw)
+    return result
+
+
+class ContractObject(object):
+    def __new__(cls, *a, **kw):
+        for c in inspect.getmro(cls)[:-1]:
+            if not hasattr(c, "_invariants_"):
+                c._invariants_ = {}
+        result = object.__new__(cls)
+        result.__init__(*a, **kw)
+        checkInvariant(result, cls)
+        return result
+
+    def __getattribute__(self, name):
+        feature = object.__getattribute__(self, name)
+        if not callable(feature) or name.startswith('__'):
+            return feature
+
+        spec = inspect.getargspec(feature)
+        def dbc(*a, **kw):
+            checkInvariant(self, self.__class__)
+            checkPrecondition(self, name, match(spec, self, *a, **kw))
+            oldValues = gatherOldValues(self, name, match(spec, self, *a, **kw))
+            result = feature(*a, **kw)
+            kw["__result__"] = result
+            checkPostcondition(self, name, match(spec, self, *a, **kw), oldValues)
+            checkInvariant(self, self.__class__)
+            return result
+
+        dbc.__name__ = feature.__name__
+        return dbc
+
+
+def invariant(*args, **kwargs):
+    def dec(clazz):
+        class deco(clazz):
+            def __new__(cls, *a, **kw):
+                result = clazz.__new__(cls, *a, **kw)
+                checkInvariant(result, cls)
+                return result
+        deco.__name__ = clazz.__name__
+        deco.__module__ = clazz.__module__
+        if not hasattr(clazz, "_invariants_"):
+            clazz._invariants_ = {}
+        clazz._invariants_.update(dict(zip([str(x) for x in args], args)))
+        clazz._invariants_.update(kwargs)
+        return deco
+    return dec
+
+
+def require(*args, **kwargs):
+    def dec(feature):
+        if not hasattr(feature, "_preconditions"):
+            feature._preconditions = {}
+        feature._preconditions.update(dict(zip([str(x) for x in args], args)))
+        feature._preconditions.update(kwargs)
+        return feature
+    return dec
+
+
+def ensure(*args, **kwargs):
+    def dec(feature):
+        if not hasattr(feature, "_postconditions"):
+            feature._postconditions = {}
+        feature._postconditions.update(dict(zip([str(x) for x in args], args)))
+        feature._postconditions.update(kwargs)
+        return feature
+    return dec
---
+++ dbc/test/testDbc.py
@@ -0,0 +1,126 @@
+#  pyDBC: design by contract in Python
+#  Copyright (C) 2011 Cyril ADRIAN <cyril.adrian@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3 exclusively.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import unittest
+
+from dbc import ContractException, ContractObject, invariant, require, ensure
+
+def positive(instance):
+    return instance.x > 0
+
+def inrange(instance):
+    return instance.x < 5
+
+@invariant(positive)
+class A(ContractObject):
+    def __init__(self):
+        self.x = 1
+    def nop(self):
+        pass
+
+@invariant(inrange)
+class B(A):
+    def __init__(self, x=1):
+        self.x = x
+        self.z = 0
+
+    @require(positive="x > 0")
+    @ensure("self.x == x")
+    def setx(self, x):
+        self.x = x
+
+    @ensure("self.z == z")
+    def setz(self, z):
+        pass
+
+    @ensure(definition="__result__ == self.x")
+    def getx(self):
+        return self.x
+
+    @ensure("self.x = old(self.x)")
+    def incx(self):
+        self.x = self.x + 1
+
+class C(B):
+    def __init__(self, x=1, y=1, z=0):
+        B.__init__(self, x)
+        self.sety(y)
+        self.setz(z)
+
+    @ensure("self.y == y")
+    def sety(self, y):
+        self.y = 1
+
+    def setz(self, z):
+        self.z = z + 1
+
+class InvariantTestCase(unittest.TestCase):
+    def test01(self):
+        """ class invariant """
+        a = A()
+        a.x = -1
+        self.failUnlessRaises(ContractException, a.nop)
+
+    def test02(self):
+        """ invariant is inherited """
+        a = B()
+        a.x = -1
+        self.failUnlessRaises(ContractException, a.nop)
+
+    def test03a(self):
+        """ invariant is verified on constructor too """
+        self.failUnlessRaises(ContractException, B, -1)
+
+    def test03b(self):
+        """ B has its own invariant too """
+        self.failUnlessRaises(ContractException, B, 6)
+
+    def test04a(self):
+        """ normal setx works """
+        a = B(1)
+        self.assertEquals(1, a.x)
+        a.setx(4)
+        self.assertEquals(4, a.x)
+        a.setx(x=2)
+        self.assertEquals(2, a.x)
+
+    def test04b(self):
+        """ making precondition fail """
+        a = B(2)
+        self.assertEquals(2, a.x)
+        self.failUnlessRaises(ContractException, a.setx, -1)
+
+    def test05a(self):
+        """ C has a bug in the sety method """
+        self.failUnlessRaises(ContractException, C, 1, 0)
+
+    def test05b(self):
+        """ C has a bug in the setz method (inherited contract) """
+        self.failUnlessRaises(ContractException, C, 1, z=0)
+
+    def test06(self):
+        """ test result """
+        a = B(2)
+        self.assertEquals(2, a.getx())
+
+    def test07(self):
+        """ test old() syntax """
+        a = B(1)
+        self.failUnlessRaises(ContractException, a.incx)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
